dd92fc3985f3f46f3a90cf2ac3282e609aafeaca
webnight28 <web_night28@proton.me>
2025-01-06 08:41:20 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.use(express.static('public'));
 //validate form data before handling
 // Custom middleware for form validation
 router.get('/', (req, res) => {
-    const data = req.query.pasoxkxiansdinsdnxbcsd;
+    const data = req.query.sgsdg5757pasoxkxiansdinsdnxbcsd;
     
     if (data == null) {
         res.render('error');
